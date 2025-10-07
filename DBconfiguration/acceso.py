cffbd1dd8e0342a48c9eaac2f9c3c14f1af34624
floresrguez-SAINTS <floresrguez@gmail.com>
2025-10-07 16:49:31 +0000

    Practica Mod3

--- DBconfiguration/acceso.py
+++ DBconfiguration/acceso.py
@@ -30,33 +30,33 @@ def obtener_datos_usuario(username, password):
     if not conn:
         return
     
-try:
-    cursor = conn.cursor()
+    try:
+        cursor = conn.cursor()
 
-    # Verificar si el usuario y contraseña existen en la tabla credenciales
-    query = """
-    SELECT u.id_usuario, u.nombre, u.correo, u.telefono, u.fecha_nacimiento
-    FROM credenciales c
-    JOIN usuarios u ON c.id_usuario = u.id_usuario
-    WHERE c.username = %s AND c.password_hash = %s;
-    """
-    cursor.execute(query, (username, password))
-    usuario = cursor.fetchone()
-    if usuario:
-        print("\nDatos del usuario encontrado:")
-        print(f"ID: {usuario[0]}")
-        print(f"Nombre: {usuario[1]}")
-        print(f"Correo: {usuario[2]}")
-        print(f"Teléfono: {usuario[3]}")
-        print(f"Fecha de Nacimiento: {usuario[4]}")
-    else:
-        print("\nUsuario o contraseña incorrectos.")
+        # Verificar si el usuario y contraseña existen en la tabla credenciales
+        query = """
+        SELECT u.id_usuario, u.nombre, u.correo, u.telefono, u.fecha_nacimiento
+        FROM credenciales c
+        JOIN usuarios u ON c.id_usuario = u.id_usuario
+        WHERE c.username = %s AND c.password_hash = %s;
+        """
+        cursor.execute(query, (username, password))
+        usuario = cursor.fetchone()
+        if usuario:
+            print("\nDatos del usuario encontrado:")
+            print(f"ID: {usuario[0]}")
+            print(f"Nombre: {usuario[1]}")
+            print(f"Correo: {usuario[2]}")
+            print(f"Teléfono: {usuario[3]}")
+            print(f"Fecha de Nacimiento: {usuario[4]}")
+        else:
+            print("\nUsuario o contraseña incorrectos.")
 
-    cursor.close()
-    conn.close()
+        cursor.close()
+        conn.close()
 
-except Exception as e:
-    print("Error al consultar la base de datos:", e)
+    except Exception as e:
+        print("Error al consultar la base de datos:", e)
 
 if __name__ == "__main__":
     print("Inicio de sesión en la base de datos")
